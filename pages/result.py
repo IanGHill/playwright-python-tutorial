4fa1d289d6294a5a38d83984beee858ca1c65248
Ian Hill <ian.g.hill@outlook.com>
2024-06-18 22:47:10 +0100

    add parameterization

--- pages/result.py
+++ pages/result.py
@@ -10,10 +10,10 @@ class DuckDuckGoResultPage:
         self.search_input = page.locator("#search_form_input")
 
     def result_link_titles(self) -> List[str]:
-        self.result_links.nth(12).wait_for()
+        self.result_links.nth(4).wait_for()
         return self.result_links.all_text_contents()
 
     def result_link_titles_contain_phrase(self, phrase: str, minimum: int = 1) -> bool:
         titles = self.result_link_titles()
-        matches = [t for t in titles if "panda" in phrase.lower()]
+        matches = [t for t in titles if phrase.lower() in t.lower()]
         return len(matches) >= minimum
--- tests/test_search_pom_fixtures.py
+++ tests/test_search_pom_fixtures.py
@@ -1,25 +1,32 @@
+import pytest
 from pages.search import DuckDuckGoSearchPage
 from pages.result import DuckDuckGoResultPage
 from playwright.sync_api import Page, expect
 
-# uses POM with fixtures
+# uses POM with fixtures and parameterise
+
+ANIMALS = ["panda", "peacock", "python", "pangolin", "parrot", "panther"]
 
 
+@pytest.mark.parametrize("phrase", ANIMALS)
 def test_basic_duckduckgo_search(
-    page: Page, search_page: DuckDuckGoSearchPage, result_page: DuckDuckGoResultPage
+    phrase: str,
+    page: Page,
+    search_page: DuckDuckGoSearchPage,
+    result_page: DuckDuckGoResultPage,
 ) -> None:
 
     # Given the DuckDuckGo page is displayed
     search_page.load()
 
     # When the user searches for a phrase
-    search_page.search("panda")
+    search_page.search(phrase)
 
     # Then the search result query is the phrase
-    expect(result_page.search_input).to_have_value("panda")
+    expect(result_page.search_input).to_have_value(phrase)
 
     # And the search result links pertain to the phrase
-    assert result_page.result_link_titles_contain_phrase("panda")
+    assert result_page.result_link_titles_contain_phrase(phrase)
 
     # And the search result title contains the phrase
-    expect(page).to_have_title("panda at DuckDuckGo")
+    expect(page).to_have_title(f"{phrase} at DuckDuckGo")
